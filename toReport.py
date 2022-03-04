a71ccdf4dfec86f297a6180fc55ddeab3816b58e
yueyangw <yueyang_wu@outlook.com>
2022-03-04 20:34:38 +0800

    qwq

--- toReport.py
+++ toReport.py
@@ -4,6 +4,11 @@ from docx import Document
 
 app = Flask(__name__)
 
+def base64ToImage(imgdata):
+    strs = imgdata[22:] #返回的base64字符串前面有几个多余字符，这里删掉
+    imgdata = base64.b64decode(strs) #把base64的图片解码成二进制返回
+    return imgdata
+
 @app.route("/")
 def mainPage():
     html = open("pie-roseType-simple.html", "r")
@@ -17,8 +22,3 @@ def saveAsImage():
         img = base64ToImage(datas['imgdata'])
         res = Response(img, mimetype="image/png")
         return res
-
-def base64ToImage(imgdata):
-    strs = imgdata[22:] #返回的base64字符串前面有几个多余字符，这里删掉
-    imgdata = base64.b64decode(strs) #把base64的图片解码成二进制返回
-    return imgdata
