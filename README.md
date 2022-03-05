55ae1ca6878522cb088d02f4ca242e40faa33e9a
yueyangw <yueyang_wu@outlook.com>
2022-03-05 17:27:53 +0800

    Update README

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # echarts-doc-generation
 通过echarts图表自动生成含有图表图片，标题，主要信息等的Word报告
+
+### 说明
+
+- template.docx为模版文档
+- 目前可使用的关键字有：
+  - \<topic>题目
+  - \<picture>图表截图
+  - \<category>图表类别
+- 添加更多关键字修改toReport.py
--- toReport.py
+++ toReport.py
@@ -1,11 +1,12 @@
-import base64, json
+import base64
+import json
 import os
-
-from flask import Flask, request, send_from_directory
-from docx import Document
-from docx.shared import Cm, Pt
 from io import BytesIO
-from docx.enum.text import WD_ALIGN_PARAGRAPH
+
+from docx import Document
+from docx.enum.text import WD_PARAGRAPH_ALIGNMENT
+from docx.shared import Cm, Pt
+from flask import Flask, request, send_from_directory
 
 app = Flask(__name__)
 
@@ -22,28 +23,28 @@ def page2():
     return html.read()
 
 
-@app.route("/savedoc", methods=['POST', 'GET'])
-def saveAsDocument():
-    if request.method == 'POST':
-        datas = request.get_data()
-        datas = json.loads(datas)
-        img = base64ToImage(datas['imgdata'])
-        template = Document('./template.docx')
-        doc = docBuilder(template, img, datas['echartsData'])
-        doc.save(r'report.docx')
-        dic = os.getcwd()
-        res = send_from_directory(dic, 'report.docx', as_attachment=True)
-        return res
+@app.route("/savedoc", methods=['POST'])
+def save_as_document():
+    datas = request.get_data()
+    datas = json.loads(datas)
+    img = base64_to_image(datas['imgdata'])
+    template = Document('./template.docx')
+    doc = doc_builder(template, img, datas['echartsData'])
+    doc.save(r'report.docx')
+    dic = os.getcwd()
+    res = send_from_directory(dic, 'report.docx', as_attachment=True)
+    return res
 
 
-def base64ToImage(imgdata):
+def base64_to_image(imgdata):
     strs = imgdata[22:]  # 返回的base64字符串前面有几个多余字符，这里删掉
     imgdata = base64.b64decode(strs)  # 把base64的图片解码成二进制返回
     return imgdata
 
 
-def docBuilder(doc, img, datas):
-    basicInfo = datas['series'][0]
+def doc_builder(doc, img, datas):
+    basic_info = datas['series'][0]
+    # mapp通过type关键字识别图表类型
     mapp = {
         'pie': '饼图',
         'line': '折线图',
@@ -53,33 +54,43 @@ def docBuilder(doc, img, datas):
     try:
         title = datas['title']['text']
     except:
-        title = basicInfo['name']
+        title = basic_info['name']
     datas = {
         'topic': title,
         'picture': img,
-        'category': mapp[basicInfo['type']],
-        'data': basicInfo['data']
+        'category': mapp[basic_info['type']],
+        'data': basic_info['data']
     }
-    replaceText(doc, "<topic>", datas['topic'])
-    replaceText(doc, "<picture>", img)
-    replaceText(doc, "<category>", datas['category'])
+    # 替换标题，图片，类别（可以按需增加）
+    replace_text(doc, "<topic>", datas['topic'])
+    replace_text(doc, "<picture>", img)
+    replace_text(doc, "<category>", datas['category'])
     return doc
 
 
-def replaceText(doc, oldText, newText):
+'''
+replace_text:
+doc是要使用的docx模版
+oldText为要更改的旧文本
+newText为替换的新文本（当old为picture时，new为二进制图片流）
+'''
+
+
+def replace_text(doc, old_text, newText):
     for para in doc.paragraphs:
-        if oldText in para.text:
-            if oldText == '<picture>':
-                para.alignment = WD_ALIGN_PARAGRAPH.CENTER
-                para.text = para.text.replace(oldText, 'pppiiictturree')
+        if old_text in para.text:
+            if old_text == '<picture>':
+                para.alignment = WD_PARAGRAPH_ALIGNMENT.CENTER
+                para.text = para.text.replace(old_text, 'pppiiictturree')
                 for run in para.runs:
                     if 'pppiiictturree' in run.text:
                         run.text = run.text.replace('pppiiictturree', '')
                         run.add_picture(BytesIO(newText), width=Cm(14))
             else:
-                para.text = para.text.replace(oldText, newText)
+                para.text = para.text.replace(old_text, newText)
                 for run in para.runs:
-                    if oldText == '<topic>':
+                    # 如果是标题，替换为宋体字号为27
+                    if old_text == '<topic>':
                         run.font.name = '宋体'
                         run.font.size = Pt(27)
                     else:
